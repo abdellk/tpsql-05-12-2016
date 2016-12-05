e034b429d7c8c722e3e0a1624aa4216f06ac4cb3
invite <invite@linux13.btssio>
2016-12-05 11:46:14 +0100

    tp 2016

--- fonctiontp/src/fonctionDerby/Fonctionchange.java
+++ fonctiontp/src/fonctionDerby/Fonctionchange.java
@@ -6,5 +6,10 @@ public class Fonctionchange {
 		
 		double resultat = prix * 1.3;
 		return resultat;
+		
+		// CALL SQLJ.install_jar('/home/invite/Bureau/Batif SLAM/Dossier JAR/fonctionChange.jar',
+		//'APP.fonctionChange', 0);
+
+
 	}
 }
