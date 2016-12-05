4686b867d9b3479e26490e3df1bf024066a133b9
invite <invite@linux13.btssio>
2016-12-05 12:04:48 +0100

    fonction change D la deuxieme

---
+++ fonctiontp/src/fonctionDerby2/FonctionchangeD.java
@@ -0,0 +1,20 @@
+package fonctionDerby2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FonctionchangeD {
+	
+	public static double changeD(double prix, String devise) throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:default:connection");
+		Statement s1 = conn.createStatement();
+		ResultSet rs = s1.executeQuery("select taux from DEVISE where devise ='"+devise+"'");
+		 rs.next();
+		double taux = rs.getDouble(1);
+		double resultat = prix*taux;
+		return resultat;
+		}
+}
